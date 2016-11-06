258b7cba90cebd7dcdee2690c8c15f647f643a2d
Duely Yung <dyung2@gmail.com>
2016-11-06 09:56:04 -0600

    Update: Updating product is working!

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ E-Commerce Website
 Spring MVC, MySQL, Hibernate, Netbeans    
     
 Broken:
-   - Uploading pictures
+   - Uploading pictures (Might need to create a bean in applicationContext)
--- src/main/java/com/se452/easyshop/controller/HomeController.java
+++ src/main/java/com/se452/easyshop/controller/HomeController.java
@@ -90,11 +90,40 @@ public class HomeController {
         return "redirect:/admin/productInventory"; 
      } 
     
-    @RequestMapping("admin/productInventory/deleteProduct/{id}")
+    @RequestMapping("/admin/productInventory/deleteProduct/{id}")
     public String deleteProduct(@PathVariable String id, Model model){
         productDao.deleteProduct(Integer.parseInt(id));
         
         return "redirect:/admin/productInventory";         
+    }
+    
+    @RequestMapping("/admin/productInventory/editProduct/{id}")
+    public String editProduct(@PathVariable String id, Model model) {
+        Product product = productDao.getProductById(Integer.parseInt(id));
+        
+        model.addAttribute(product);
+        
+        return "editProduct";
+    }
+    
+    @RequestMapping(value="/admin/productInventory/editProduct", method = RequestMethod.POST)
+    public String editProduct(@ModelAttribute("product") Product product, Model model, HttpServletRequest request) {
+          productDao.editProduct(product); /* DELETE this when image upload is fixed*/
+          
+//        MultipartFile productImage = product.getProductImage();
+//        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+//        path = Paths.get(rootDirectory + "\\WEB-INF\\images\\" + product.getProductId() + ".png");
+//
+//        if (productImage != null && !productImage.isEmpty()) {
+//            try {
+//                productImage.transferTo(new File(path.toString));
+//            } catch (Exception e) {
+//                throw new RuntimeException("Save failed", e);
+//            }
+//
+//            productDao.editProduct(product);
+//        }
+        return "redirect:/admin/productInventory";
     }
     
 }
